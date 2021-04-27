b3b421fd27432365d6fb4d13296a96374c0b2a99
kobi wolf <kobiwolf95@gmail.com>
2021-04-28 00:02:59 +0300

    add route get

--- db/mongoDB.js
+++ db/mongoDB.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const path = 'mongodb://myUserAdmin:abc789@18.197.28.167:27017/';
-mongoose.connect(path, () => {
-  console.log('we are online');
-});
+mongoose.connect(
+  path,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('we are online');
+  }
+);
